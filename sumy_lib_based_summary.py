57aa5436641c9cbc3c8a1b0f22d3fd736fd621ce
shubham Mandowara <shubham77mandowara@gmail.com>
2023-09-03 19:41:26 +0530

    wip: Debugging

--- sumy_lib_based_summary.py
+++ sumy_lib_based_summary.py
@@ -4,12 +4,10 @@ from sumy.parsers.plaintext import PlaintextParser
 from sumy.summarizers.text_rank import TextRankSummarizer
 from sumy.nlp.tokenizers import Tokenizer
 from helper.helper import convert_text_to_lowercase
-from streamlit.logger import get_logger
 import nltk
 from typing import List
 
 nltk.download("punkt")
-logger = get_logger(__name__)
 
 
 def common_process(text: str)-> str:
@@ -39,7 +37,6 @@ def main(text:str, model_name:str, sentence_on_output:int=2) -> str:
             str: Summary of text
     """
     summarizer = None
-    get_logger.info(model_name)
     if model_name == 'Lex Rank':
         summarizer = LexRankSummarizer()
     elif model_name == 'LSA':
@@ -49,10 +46,7 @@ def main(text:str, model_name:str, sentence_on_output:int=2) -> str:
     else:
         raise 'Wrong model name / Model name is not defined'
     parser = common_process(text=text)
-    get_logger.info('parsing is done')
     summary = summarizer(parser.document,sentence_on_output)
-    get_logger.info(summary)
     return_text = common_return_process(summary=summary)
-    get_logger.info(return_text)
     return return_text
 
