4c805ed2eb51ba59e1be5480f42ea80512c17e18
shubham Mandowara <shubham77mandowara@gmail.com>
2023-09-03 22:55:46 +0530

    linkedin link

--- streamlit.py
+++ streamlit.py
@@ -14,9 +14,9 @@ if __name__ == "__main__":
     st.sidebar.header("Select a model from the list 👇🏻:")
     
     st.sidebar.markdown('Follow me on linked to know more about AI, ML, DL, Generative AI, Deployment and MLOps')
-    column1, column2 = st.columns(2)
-    column1.markdown("[![Linkedin](https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg])(https://www.linkedin.com/in/shubhammandowara/)")
-    column2.markdown("[![Linkedin](https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg])(https://www.linkedin.com/in/shubhammandowara/)")
+    column1, column2 = st.sidebar.columns(2)
+    column1.markdown("[![Linkedin](https://static-00.iconduck.com/assets.00/linkedin-icon-512x512-vkm0drb1.png)(https://www.linkedin.com/in/shubhammandowara/)")
+    column2.markdown("[![Linkedin](https://static-00.iconduck.com/assets.00/linkedin-icon-512x512-vkm0drb1.png)(https://www.linkedin.com/in/shubhammandowara/)")
     model_selection = st.sidebar.selectbox(
         "Choose a model",
         [
